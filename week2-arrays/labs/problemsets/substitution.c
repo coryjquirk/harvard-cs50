a5478d40ecb1ce59b57223cc92fd2f366cfa1b48
Cory Quirk <cory.quirk@calabrio.com>
2021-08-27 18:56:21 -0500

    validating cipherkey

--- week2-arrays/labs/problemsets/substitution.c
+++ week2-arrays/labs/problemsets/substitution.c
@@ -20,42 +20,67 @@ int main(int argc, string argv[])
     bool validKey = validateKey(cipherKey);
     if (!validKey)
     {
-        printf("Usage: ./substitution key\n");
+    printf("Usage: ./substitution key\n");
     }
     else
     {
-        //prompt user for string
-        string plaintext = get_string("plaintext to encipher: \n");
-        //print cipher text
-        printf("ciphertext: %s\n", "blah\n");
-        //ENCIPHER !@D2309D23D@#d23D2903DJ230D23D2#d@#d@#2982F209
+    //prompt user for string
+    string plaintext = get_string("plaintext to encipher: \n");
 
+    //ENCIPHER !@D2309D23D@#d23D2903DJ230D23D2#d@#d@#2982F209
+        //CASING: upper stays upper, lower stays lower
         //NONALPHA: punctuation and spaces stay the same from plaintext->ciphertext.
+
+    //print cipher text
+    printf("ciphertext: %s\n", "blah\n");
     }
 }
 
 int validateKey(string cipherKey)
 {
+    //test keys
+    //ytnshkvefxrbauqzclwdmipgjo
+    //YTNSHKVEFXRBAUQZCLWDMIPGJO
+    //yTnShKvEfXrBaUqZcLwDmIpGj
+
     //initial key validation:
-    //REPETITION: no repeat characters
-    //CASING: upper stays upper, lower stays lower
 
     //LENGTH: must be 26 characters
     if (strlen(cipherKey) != 26)
     {
-        printf("Key must contain 26 characters.\n");
+    printf("Key must contain 26 characters.\n");
     }
     else
     {
         for (int i = 0; i < strlen(cipherKey); i++)
         {
+            char character = cipherKey[i];
             //TYPE: only letters
-            if (!isalpha(cipherKey[i]))
+            if (!isalpha(character))
             {
                 printf("Key must contain letters only.\n");
                 break;
             }
+            else
+            {
+                //REPETITION: no repeat characters
+                //through length of cipherKey
+                if (islower(character))
+                {
+                    //convert all 26 char to upper
+                    char upperChar = toupper(character);
+                    cipherKey[i] = upperChar;
+                }
+                else
+                {
+                    //run through all characters
+                    //make sure cipherKey[i] value appears only once
+                    //if repeat value is found, break with error message
+                }
+                //else, continue
+            }
+            printf("uppercase key: %s\n", cipherKey);
         }
     }
-    return 0;
+    return 1;
 }
